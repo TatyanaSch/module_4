084783ab92be2b0f6c1a5d73a4004e0cac9aed14
TatyanaSch <tatyana.s@sinorutrans.com>
2024-11-21 13:21:53 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,9 @@
+def divide(first, second):
+    if second == 0:
+        return 'Ошибка'
+    else:
+        return first / second
+
+
+# print(divide(3, 0))
+# print(divide(25, 6))
---
+++ true_math.py
@@ -0,0 +1,12 @@
+from math import inf
+
+
+def divide(first, second):
+    if second == 0:
+        return inf
+    else:
+        return first / second
+
+
+# print(divide(1, 0))
+# print(divide(50, 25))
